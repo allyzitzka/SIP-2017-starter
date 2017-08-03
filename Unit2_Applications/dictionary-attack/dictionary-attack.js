d558484b3f074728b70fc334c87204b32241567b
allyzitzka <allykitkaat@gmail.com>
2017-08-03 13:23:15 -0700

    my

--- Unit2_Applications/dictionary-attack/dictionary-attack.js
+++ Unit2_Applications/dictionary-attack/dictionary-attack.js
@@ -1,5 +1,6 @@
 var wordsList = [];
 
+
 function init() {
   // Load the words from the dictionary text file to wordsList
   var wordsFile = "https://raw.githubusercontent.com/GirlsFirst/SIP-2017/master/Unit2_Applications/dictionary-attack/dictionary.txt?token=ADcVhZjRMd86ZdhPE2jVvIaJdQdzLA6Yks5YvvVSwA%3D%3D";
